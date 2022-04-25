1f40b7b0f55d168075c7ad12c371740906b8c4f3
persiopeo <persiopeo@hotmail.com>
2022-04-25 18:49:22 -0300

    Requisito-02 passando lint e iniciando 03

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,10 +3,16 @@ import { StarContexto } from '../contexto/Context';
 
 function Filter() {
   const { data, setFilterPlanets } = useContext(StarContexto);
+  const [currentFilters, setCurrentFilters] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: 0,
+  });
   const [filter, setFilter] = useState({
     filterByName: {
       name: '',
     },
+    filterByNumericValues: [],
   });
   useEffect(() => setFilterPlanets(
     data.filter(
@@ -16,20 +22,77 @@ function Filter() {
     ),
   ), [data, filter, setFilterPlanets]);
 
+  const onChange = ({ target: { name, value } }) => {
+    setCurrentFilters((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setFilter((prevState) => ({
+      ...prevState,
+      filterByNumericValues: [
+        ...prevState.filterByNumericValues,
+        currentFilters,
+      ],
+    }));
+  };
   return (
-    <section>
-      <h2>Projeto Star Wars - Trybe</h2>
-      <input
-        type="text"
-        data-testid="name-filter"
-        onChange={ ({ target: { value } }) => setFilter((prevState) => ({
-          ...prevState,
-          filterByName: { name: value },
-        })) }
-      />
+    <>
+      <section>
+        <h2>Projeto Star Wars - Trybe</h2>
+        <input
+          type="text"
+          data-testid="name-filter"
+          onChange={ ({ target: { value } }) => setFilter((prevState) => ({
+            ...prevState,
+            filterByName: { name: value },
+          })) }
+        />
 
-    </section>
-
+      </section>
+      <section>
+        <form>
+          <select
+            data-testid="column-filter"
+            name="column-filter"
+            onChange={ onChange }
+            defaultValue="population"
+          >
+            <option value="population">population</option>
+            <option value="orbital_period">orbital_period</option>
+            <option value="diameter">diameter</option>
+            <option value="rotation_period">rotation_period</option>
+            <option value="surface_water">surface_water</option>
+          </select>
+          <select
+            data-testid="comparison-filter"
+            name="comparison-filter"
+            onChange={ onChange }
+            defaultValue="maior que"
+          >
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
+          </select>
+          <input
+            data-testid="value-filter"
+            type="number"
+            name="value-filter"
+            onChange={ onChange }
+            defaultValue="0"
+          />
+          <button
+            type="submit"
+            data-testid="button-filter"
+            onClick={ onSubmit }
+          >
+            Filtrar
+          </button>
+        </form>
+      </section>
+    </>
   );
 }
 
